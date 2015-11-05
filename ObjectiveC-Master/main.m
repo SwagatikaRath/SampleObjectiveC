dd59d96e8b0776de0a28bbfe823191873f9ae7a5
Swagatika <reetu.swagatika@gmail.com>
2015-11-05 11:57:45 -0600

    Initial Commit

---
+++ ObjectiveC-Master/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjectiveC-Master
+//
+//  Created by Swagatika on 11/5/15.
+//  Copyright © 2015 swagatika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ObjectiveC-MasterTests/ObjectiveC_MasterTests.m
@@ -0,0 +1,39 @@
+//
+//  ObjectiveC_MasterTests.m
+//  ObjectiveC-MasterTests
+//
+//  Created by Swagatika on 11/5/15.
+//  Copyright © 2015 swagatika. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ObjectiveC_MasterTests : XCTestCase
+
+@end
+
+@implementation ObjectiveC_MasterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
